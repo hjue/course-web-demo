0075f88a2d77d6565be6a8715a95f9e499053888
Jason <haojue@gmail.com>
2021-09-14 18:07:16 +0800

    支持代码高亮

--- src/components/Doc.vue
+++ src/components/Doc.vue
@@ -12,8 +12,13 @@
 <script>
 // import vue from '@/api'
 import axios from 'axios'
+// 课程内容样式，来自typora
 import '@/assets/article/typora-lark.css'
 // import '@/assets/article/github.css'
+// 代码高亮样式
+import hljs from 'highlight.js'
+import 'highlight.js/styles/atom-one-dark.css'
+
 import SingleChoiceQuestionVue from './SingleChoiceQuestion.vue'
 import MultipleChoiceQuestion from './MultipleChoiceQuestion.vue'
 import CodeQuestion from './CodeQuestion.vue'
@@ -25,6 +30,18 @@ Vue.component('MultipleChoiceQuestion', MultipleChoiceQuestion)
 Vue.component('CodeQuestion', CodeQuestion)
 
 const marked = require('marked')
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  highlight: code => hljs.highlightAuto(code).value,
+  pedantic: false,
+  gfm: true,
+  tables: true,
+  breaks: false,
+  sanitize: false,
+  smartLists: true,
+  smartypants: false,
+  xhtml: false
+})
 
 export default {
   components: { SingleChoiceQuestionVue, MultipleChoiceQuestion, CodeQuestion },
@@ -52,8 +69,7 @@ export default {
       axios.get(url).then(response => {
         console.log(response.data)
         // markdown文档转为html
-        const html =
-          '<div>' + marked(response.data.md, { sanitize: false }) + '</div>'
+        const html = '<div>' + marked(response.data.md) + '</div>'
 
         // 这里是把文稿规范里的标签替换为vue组件
         let htmlContainer = html.replace(
@@ -70,6 +86,8 @@ export default {
         )
         // htmlContainer = htmlContainer.replace(/<question id="(\d+)"><\/question>/g, '<SingleChoiceQuestionVue qid="$1" />')
         // console.log(htmlContainer)
+        // 处理代码高亮
+        htmlContainer = htmlContainer.replace(/<pre>/g, "<pre class='hljs'>")
         this.docHtml = htmlContainer
       })
     }
@@ -84,9 +102,6 @@ export default {
 #write code {
   display: inline-block;
   width: 98%;
-  border: 1px solid #e7eaed;
-  background-color: #f8f8f8;
-  border-radius: 3px;
   padding: 10px 10px 10px 10px;
   font-size: 0.9em;
   word-wrap: break-word;
